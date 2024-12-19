290585361656cce2bda6ca659cb2d85f8399c001
Mochama Adriano <mochama254@protonmail.com>
2024-12-19 14:06:18 +0300

    feat: add codetrail config get command (#10)

--- tests/test_cmd_config.py
+++ tests/test_cmd_config.py
@@ -1,8 +1,9 @@
+import logging
 import pytest
 
 from codetrail import commands
 from codetrail import exceptions
-from codetrail.cmd_config import set_config
+from codetrail.cmd_config import get_config, set_config
 
 
 def initialize_repository_command(path):
@@ -44,3 +45,34 @@ class TestSetConfig:
         """Test with wrong option."""
         with pytest.raises(exceptions.UnsupportedConfigError):
             set_config(commands.SetConfig(key="user.names", value="Chill Guy"))
+
+
+@pytest.mark.usefixtures("default_path")
+class TestGetConfig:
+    """Tests for `get_config` function."""
+
+    def test_can_get_name_for_a_user(self, code_repository, config_parser, caplog):
+        """Test gets user name from config."""
+        set_config(commands.SetConfig(key="user.name", value="Chill Guy"))
+
+        with caplog.at_level(logging.INFO):
+            get_config(commands.GetConfig(key="user.name"))
+            assert "Chill Guy" in caplog.text
+
+    def test_raises_exception_on_unsupported_section(
+        self,
+        code_repository,
+        config_parser,
+    ):
+        """Test with wrong section."""
+        with pytest.raises(exceptions.UnsupportedConfigError):
+            get_config(commands.GetConfig(key="users.name"))
+
+    def test_raises_exception_on_unsupported_option(
+        self,
+        code_repository,
+        config_parser,
+    ):
+        """Test with wrong option."""
+        with pytest.raises(exceptions.UnsupportedConfigError):
+            get_config(commands.GetConfig(key="user.names"))
